5e2ebc54ac9c503d976543dbbab2c6ba2211fefc
vsavin <savinvitya@gmail.com>
2021-04-11 20:50:39 +0300

    feat: add 11-encode-line implementation

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 1) {
+    return str;
+  }
+  const res = [];
+  let counter = 1;
+  let prev = str[0];
+
+  for (let idx = 1; idx < str.length + 1; idx++) {
+    const cur = str[idx];
+
+    if (cur === prev) {
+      counter += 1;
+    } else {
+      res.push(counter === 1 ? prev : `${counter}${prev}`);
+      prev = cur;
+      counter = 1;
+    }
+  }
+
+  return res.join('');
 }
 
 module.exports = encodeLine;
